c1bbc7e2f663bdfc303b805b436444aeef9eb51b
Lucas Yuzo Kihana <lucas@TARS.local>
2024-09-01 18:26:32 -0300

    chore: set up applications

---
+++ payment/src/test/java/com/fiap/tech_challenge_5/payment/PaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.tech_challenge_5.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ product/src/test/java/com/fiap/tech_challenge_5/product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.tech_challenge_5.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
