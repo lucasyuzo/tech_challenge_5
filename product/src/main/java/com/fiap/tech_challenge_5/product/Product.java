86c703154aaa3d966da9c39b5410b67a664a924a
Lucas Yuzo Kihana <lucas@TARS.local>
2024-09-03 10:35:32 -0300

    feat: product crud

---
+++ product/src/main/java/com/fiap/tech_challenge_5/product/Product.java
@@ -0,0 +1,56 @@
+package com.fiap.tech_challenge_5.product;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "product_table")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+    private String name;
+    private Double price;
+    private Integer quantity;
+
+    public Product() {}
+
+    public Product(Integer id, String name, Double price, Integer quantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
